7972a14ae071a23700c3c188b3703536782fc872
Sam Szreter <s.szreter@gmail.com>
2017-11-30 16:13:56 +0200

    Remove test

--- src/main/java/httpserver/App.java
+++ src/main/java/httpserver/App.java
@@ -2,10 +2,8 @@ package httpserver;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-
-    public String message() {
-        return "Hello World";
+        int portNumber = Integer.parseInt(args[0]);
+        String fileDirectory = args[1];
+        
     }
 }
--- src/test/java/httpserver/AppTest.java
+++ src/test/java/httpserver/AppTest.java
@@ -5,9 +5,4 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test
-    public void returnsHelloWorld() throws Exception {
-        App app = new App();
-        assertEquals(app.message(), "Hello World");
-    }
 }
