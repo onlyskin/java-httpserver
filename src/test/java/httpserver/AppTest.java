248376129fbe9be140768f6e35a8d5beab9dbb0a
Sam Szreter <s.szreter@gmail.com>
2017-11-30 12:52:35 +0200

    Add a failing test

---
+++ src/test/java/httpserver/AppTest.java
@@ -0,0 +1,13 @@
+package httpserver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void returnsHelloWorld() throws Exception {
+        App app = new App();
+        assertEquals(app.message(), "hello World");
+    }
+}
