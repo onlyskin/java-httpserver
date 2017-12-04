e72c4190b6b6d2942ae17780aa5ba52be49364be
Sam Szreter <s.szreter@gmail.com>
2017-12-04 12:59:46 +0200

    Implement Server class with an integration test to tie together the parse, respond, and writeresponse operations

--- src/main/java/httpserver/Server.java
+++ src/main/java/httpserver/Server.java
@@ -1,23 +1,25 @@
 package httpserver;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStream;
 
 public class Server {
-    private final int port;
-    private final ServerSocket serverSocket;
+    private final String fileDirectory;
+    private final RequestParser requestParser;
+    private final Responder responder;
+    private final ResponseWriter responseWriter;
 
-    public Server(int port, String fileDirectory) throws IOException {
-        this.port = port;
-
-        this.serverSocket = new ServerSocket(port);
+    public Server(String fileDirectory) {
+        this.fileDirectory = fileDirectory;
+        this.requestParser = new RequestParser();
+        this.responder = new Responder(fileDirectory);
+        this.responseWriter = new ResponseWriter();
     }
 
-    public void run() throws IOException {
-        serverSocket.accept();
-    }
-
-    public void close() throws IOException {
-        serverSocket.close();
+    public void process(InputStream inputStream, ByteArrayOutputStream outputStream) throws IOException {
+        Request request = requestParser.parse(inputStream);
+        Response response = responder.makeResponse(request);
+        responseWriter.write(response, outputStream);
     }
 }
--- src/test/java/httpserver/ServerTest.java
+++ src/test/java/httpserver/ServerTest.java
@@ -1,10 +1,8 @@
 package httpserver;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.*;
-import java.net.Socket;
 
 import static org.junit.Assert.*;
 
@@ -15,51 +13,23 @@ public class ServerTest {
         this.tempdir = System.getProperty("java.io.tmpdir");
     }
 
-    @Ignore
     @Test
-    public void writesRequestedFileContentsToSocketOutForGET() throws Exception {
+    public void writesRequestedFileContentsToOutputStreamForGET() throws Exception {
         File tempFile = File.createTempFile("temp-", "-testfile");
         tempFile.deleteOnExit();
         String fullPath = tempFile.toString();
         String relativePath = fullPath.substring(tempdir.length());
-
         FileOutputStream fileOutputStream = new FileOutputStream(fullPath);
         fileOutputStream.write("Test file contents for GET request.".getBytes());
 
-        Server server = new Server(5000, tempdir);
-        server.run();
+        byte[] request = ("GET " + relativePath + " HTTP/1.1\r\nHost: 127.0.0.1:5000\r\n\r\n").getBytes();
+        InputStream inputStream = new ByteArrayInputStream(request);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Server server = new Server(tempdir);
 
-        Thread thread = new Thread(new Runnable() {
+        server.process(inputStream, outputStream);
 
-            @Override
-            public void run() {
-                BufferedReader bufferedReader = null;
-                try (Socket clientSocket = new Socket("127.0.0.1", 5000)) {
-                    OutputStream outputStream = clientSocket.getOutputStream();
-                    String requestString = "GET " + relativePath + " HTTP/1.1\r\nHost: 127.0.0.1:5000\r\n\r\n";
-                    outputStream.write(requestString.getBytes());
-                    outputStream.flush();
-
-                    bufferedReader = new BufferedReader(
-                            new InputStreamReader(clientSocket.getInputStream()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    String line = bufferedReader.readLine();
-                    assertEquals("HTTP/1.1 200 OK", line);
-                    line = bufferedReader.readLine();
-                    assertEquals("Content-Length: 35", line);
-                    line = bufferedReader.readLine();
-                    assertEquals("", line);
-                    line = bufferedReader.readLine();
-                    assertEquals("Test file contents for GET request.", line);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
+        String expected = "HTTP/1.1 200 OK\r\nContent-Length: 35\r\n\r\nTest file contents for GET request.";
+        assertEquals(expected, outputStream.toString());
     }
 }
