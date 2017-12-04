a19e1965c110717615d7a590fad82bc0568fdedd
Sam Szreter <s.szreter@gmail.com>
2017-12-04 23:23:13 +0200

    Break methods up

--- src/main/java/httpserver/RequestParser.java
+++ src/main/java/httpserver/RequestParser.java
@@ -8,18 +8,27 @@ import java.util.HashMap;
 
 public class RequestParser {
     public Request parse(InputStream inputStream) throws IOException {
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(inputStream));
+        BufferedReader in = new BufferedReader( new InputStreamReader(inputStream));
 
         String[] requestParams = in.readLine().split(" ");
-
-        HashMap<String, String> headers = new HashMap<>();
-        String inputLine;
-        while (!(inputLine = in.readLine()).equals("")) {
-            String[] header = inputLine.split(":\\s*", 2);
-            headers.put(header[0], header[1]);
-        }
+        HashMap<String, String> headers = parseHeaders(in);
 
         return new Request(requestParams[0], requestParams[1], headers);
+    }
+
+    private HashMap<String, String> parseHeaders(BufferedReader in) throws IOException {
+        HashMap<String, String> headers = new HashMap<>();
+        String inputLine;
+
+        while (!(inputLine = in.readLine()).equals("")) {
+            parseHeader(headers, inputLine);
+        }
+
+        return headers;
+    }
+
+    private void parseHeader(HashMap<String, String> headers, String inputLine) {
+        String[] header = inputLine.split(":\\s*", 2);
+        headers.put(header[0], header[1]);
     }
 }
