c959443bb7ec067e230e212d7222ff05947e33d8
Sam Szreter <s.szreter@gmail.com>
2017-12-04 13:38:47 +0200

    Update to use abstract OutputStream instead of concrete ByteArrayOutputStream

--- src/main/java/httpserver/IoProcessor.java
+++ src/main/java/httpserver/IoProcessor.java
@@ -1,23 +1,21 @@
 package httpserver;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IoProcessor {
-    private final String fileDirectory;
     private final RequestParser requestParser;
     private final Responder responder;
     private final ResponseWriter responseWriter;
 
     public IoProcessor(String fileDirectory) {
-        this.fileDirectory = fileDirectory;
         this.requestParser = new RequestParser();
         this.responder = new Responder(fileDirectory);
         this.responseWriter = new ResponseWriter();
     }
 
-    public void process(InputStream inputStream, ByteArrayOutputStream outputStream) throws IOException {
+    public void process(InputStream inputStream, OutputStream outputStream) throws IOException {
         Request request = requestParser.parse(inputStream);
         Response response = responder.makeResponse(request);
         responseWriter.write(response, outputStream);
