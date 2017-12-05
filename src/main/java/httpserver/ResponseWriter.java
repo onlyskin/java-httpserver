4ad92ccabcafc9c78124378e3199f311e431c353
Sam Szreter <s.szreter@gmail.com>
2017-12-06 00:24:06 +0200

    Add integration test for 405 on post

--- src/main/java/httpserver/ResponseWriter.java
+++ src/main/java/httpserver/ResponseWriter.java
@@ -14,6 +14,7 @@ public class ResponseWriter {
         this.statuses = new HashMap<>();
         statuses.put(200, "OK");
         statuses.put(404, "Not Found");
+        statuses.put(405, "Method Not Allowed");
     }
 
     public void write(Response response, OutputStream outputStream) throws IOException {
