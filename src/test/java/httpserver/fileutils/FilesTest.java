9f54d2f3de8d0a95a7cbea9d775f3c73757b7047
Sam Szreter <s.szreter@gmail.com>
2017-12-06 16:29:47 +0200

    Add create, delete, append, and replace functions for Paths

--- src/test/java/httpserver/fileutils/FilesTest.java
+++ src/test/java/httpserver/fileutils/FilesTest.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static httpserver.fileutils.FileHelpers.*;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.readAllBytes;
 import static org.junit.Assert.*;
 
 public class FilesTest {
@@ -26,35 +28,6 @@ public class FilesTest {
         file2 = tempFileOptions(dir, "bbb");
         fileWithContents = fileWithContents("Temp file contents");
         nonePath = Paths.get("/var/nonePath");
-    }
-
-    @Test
-    public void makesFullPathFromRootPathAndRequestPathString() throws Exception {
-        String requestPathString = "/example.txt";
-
-        Path expected = Paths.get("tmp/serving-dir/example.txt");
-        assertEquals(expected,
-                Files.fullPathForRequestPath(root, requestPathString));
-    }
-
-    @Test
-    public void makesFullPathCorrectlyWhenRequestPathIsSlash() throws Exception {
-        String requestPathString = "/";
-
-        Path expected = Paths.get("tmp/serving-dir");
-        assertEquals(expected,
-                Files.fullPathForRequestPath(root, requestPathString));
-    }
-
-    @Test
-    public void makesRootPathFromStringInput() throws Exception {
-        String input1 = "/Users/example/public/";
-        String input2 = "/Users/example/public";
-
-        assertEquals(Paths.get("/Users", "example", "public"),
-                Files.getPath(input1));
-        assertEquals(Paths.get("/Users", "example", "public"),
-                Files.getPath(input2));
     }
 
     @Test
@@ -92,5 +65,85 @@ public class FilesTest {
 
         assertEquals(file1, actual[0]);
         assertEquals(file2, actual[1]);
+    }
+
+    @Test
+    public void createsFileAtPath() throws Exception {
+        Path path = Paths.get(dir.toString(), "/example.txt");
+        Files.createFileAtPath(path);
+
+        assertTrue(exists(path));
+    }
+
+    @Test
+    public void deletesFileAtPath() throws Exception {
+        Path path = Paths.get(dir.toString(), "/example.txt");
+        Files.createFileAtPath(path);
+        Files.deleteFileAtPath(path);
+
+        assertFalse(exists(path));
+    }
+
+    @Test
+    public void appendsToFileContentsAtPath() throws Exception {
+        Path path = Paths.get(dir.toString(), "/example.txt");
+
+        Files.createFileAtPath(path);
+        String contents = new String(readAllBytes(path));
+        assertEquals("", contents);
+
+        Files.appendToFile(path, "first line\r\n".getBytes());
+        String newContents1 = new String(readAllBytes(path));
+        assertEquals("first line\r\n", newContents1);
+
+        Files.appendToFile(path, "second line\r\n".getBytes());
+        String newContents2 = new String(readAllBytes(path));
+        assertEquals("first line\r\nsecond line\r\n", newContents2);
+    }
+
+    @Test
+    public void replacesFileContentsAtPath() throws Exception {
+        Path path = Paths.get(dir.toString(), "/example.txt");
+
+        Files.createFileAtPath(path);
+        String contents = new String(readAllBytes(path));
+        assertEquals("", contents);
+
+        Files.replaceContents(path, "first line\r\n".getBytes());
+        String newContents1 = new String(readAllBytes(path));
+        assertEquals("first line\r\n", newContents1);
+
+        Files.replaceContents(path, "second line\r\n".getBytes());
+        String newContents2 = new String(readAllBytes(path));
+        assertEquals("second line\r\n", newContents2);
+    }
+
+    @Test
+    public void makesFullPathFromRootPathAndRequestPathString() throws Exception {
+        String requestPathString = "/example.txt";
+
+        Path expected = Paths.get("tmp/serving-dir/example.txt");
+        assertEquals(expected,
+                Files.fullPathForRequestPath(root, requestPathString));
+    }
+
+    @Test
+    public void makesFullPathCorrectlyWhenRequestPathIsSlash() throws Exception {
+        String requestPathString = "/";
+
+        Path expected = Paths.get("tmp/serving-dir");
+        assertEquals(expected,
+                Files.fullPathForRequestPath(root, requestPathString));
+    }
+
+    @Test
+    public void makesRootPathFromStringInput() throws Exception {
+        String input1 = "/Users/example/public/";
+        String input2 = "/Users/example/public";
+
+        assertEquals(Paths.get("/Users", "example", "public"),
+                Files.getPath(input1));
+        assertEquals(Paths.get("/Users", "example", "public"),
+                Files.getPath(input2));
     }
 }
