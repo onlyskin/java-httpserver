9b0c18d76d9eb2ebc07301bbc65c1261a6796e58
Sam Szreter <s.szreter@gmail.com>
2017-12-06 14:06:43 +0200

    DRY up Html tests

--- src/test/java/httpserver/fileutils/HtmlTest.java
+++ src/test/java/httpserver/fileutils/HtmlTest.java
@@ -2,6 +2,7 @@ package httpserver.fileutils;
 
 import org.junit.Test;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,47 +12,44 @@ import static httpserver.fileutils.FileHelpers.tempFileOptions;
 import static org.junit.Assert.*;
 
 public class HtmlTest {
+
+    private final Path root;
+    private final Path fullDirPath;
+    private final Path fullFilePath;
+
+    public HtmlTest() throws IOException {
+        root = tempDir();
+        fullDirPath = tempDirOptions(root);
+        fullFilePath = tempFileOptions(root, "aaa");
+    }
+
     @Test
     public void makesHrefStringFromRootPathAndFullFilePath() throws Exception {
         Path root = Paths.get("/Users", "example", "public");
         Path fullFilePath = Paths.get("/Users", "example", "public", "example.txt");
 
-        String expected = "example.txt";
-        String actual = Html.hrefString(root, fullFilePath);
-        assertEquals(expected, actual);
+        assertEquals("example.txt", Html.hrefString(root, fullFilePath));
     }
 
     @Test
     public void makesHrefStringFromRootPathAndDirPath() throws Exception {
-        Path root = tempDir();
-        Path fullDirPath = tempDirOptions(root);
+        String expected = fullDirPath.toString().substring(root.toString().length() + 1) + "/";
 
-        String expected = fullDirPath.toString()
-                .substring(root.toString().length() + 1) + "/";
-        String actual = Html.hrefString(root, fullDirPath);
-        assertEquals(expected, actual);
+        assertEquals(expected, Html.hrefString(root, fullDirPath));
     }
 
     @Test
     public void makesHtmlLinkFromRootPathAndDirPath() throws Exception {
-        Path root = tempDir();
-        Path fullFilePath = tempFileOptions(root, "aaa");
-        Path fullDirPath = tempDirOptions(root);
-
         String expectedFilePath = fullFilePath.toString()
                 .substring(root.toString().length() + 1);
         String expectedDirPath = fullDirPath.toString()
                 .substring(root.toString().length() + 1) + "/";
-
         String expectedFileLink = "<div><a href=\"" + expectedFilePath +
                 "\">" + expectedFilePath + "</a></div>";
         String expectedDirLink = "<div><a href=\"" + expectedDirPath +
                 "\">" + expectedDirPath + "</a></div>";
 
-        String actualFileLink = Html.linkString(root, fullFilePath);
-        String actualDirLink = Html.linkString(root, fullDirPath);
-
-        assertEquals(expectedFileLink, actualFileLink);
-        assertEquals(expectedDirLink, actualDirLink);
+        assertEquals(expectedFileLink, Html.linkString(root, fullFilePath));
+        assertEquals(expectedDirLink, Html.linkString(root, fullDirPath));
     }
 }
