edf854f1ac648925073e8aa9b9f2f00915d70b6d
Sam Szreter <s.szreter@gmail.com>
2017-12-11 18:25:14 +0200

    Add LogsResponder

--- src/main/java/httpserver/responder/LogsResponder.java
+++ src/main/java/httpserver/responder/LogsResponder.java
@@ -1,15 +1,21 @@
 package httpserver.responder;
 
 import httpserver.AppConfig;
+import httpserver.Authorizer;
 import httpserver.Header;
 import httpserver.Request;
 import httpserver.response.OkResponse;
 import httpserver.response.Response;
+import httpserver.response.UnauthorizedResponse;
 
 public class LogsResponder implements Responder {
     @Override
     public Response respond(AppConfig appConfig, Request request) {
-        byte[] log = appConfig.getLogger().readLog();
-        return new OkResponse(log, new Header[0]);
+        if (new Authorizer().authorize(request)) {
+            byte[] log = appConfig.getLogger().readLog();
+            return new OkResponse(log, new Header[0]);
+        } else {
+            return new UnauthorizedResponse();
+        }
     }
 }
--- src/test/java/httpserver/responder/LogsResponderTest.java
+++ src/test/java/httpserver/responder/LogsResponderTest.java
@@ -1,6 +1,7 @@
 package httpserver.responder;
 
 import httpserver.AppConfig;
+import httpserver.Header;
 import httpserver.Logger;
 import httpserver.Request;
 import httpserver.response.Response;
@@ -10,17 +11,36 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class LogsResponderTest {
+
+    private final Logger loggerMock;
+    private final AppConfig appConfigMock;
+    private final LogsResponder logsResponder;
+
+    public LogsResponderTest() {
+        logsResponder = new LogsResponder();
+
+        loggerMock = mock(Logger.class);
+        appConfigMock = mock(AppConfig.class);
+        when(appConfigMock.getLogger()).thenReturn(loggerMock);
+    }
+
     @Test
     public void callsReadLogOnAppConfigLogger() {
-        Logger loggerMock = mock(Logger.class);
-        AppConfig appConfigMock = mock(AppConfig.class);
-        when(appConfigMock.getLogger()).thenReturn(loggerMock);
-        LogsResponder logsResponder = new LogsResponder();
+        Header[] headers = new Header[]{new Header("Authorization", "Basic YWRtaW46aHVudGVyMg==")};
+        Request request = new Request("GET", "test", headers);
 
-        Response response = logsResponder.respond(appConfigMock, mock(Request.class));
+        Response response = logsResponder.respond(appConfigMock, request);
 
         verify(loggerMock).readLog();
         assertEquals(200, response.getStatusCode());
     }
 
+    @Test
+    public void returnsUnauthorizedResponseWhenIncorrectAuthHeader() {
+        Request request = new Request("GET", "test", new Header[0]);
+
+        Response response = logsResponder.respond(appConfigMock, request);
+
+        assertEquals(401, response.getStatusCode());
+    }
 }
