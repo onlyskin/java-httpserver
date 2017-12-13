79f724d374b1d532a14217fc62ef5bb678f8e37b
Sam Szreter <s.szreter@gmail.com>
2017-12-13 19:25:02 +0200

    Remove Method enum extra bits

--- src/main/java/httpserver/Method.java
+++ src/main/java/httpserver/Method.java
@@ -1,21 +1,5 @@
 package httpserver;
 
-import httpserver.responder.*;
-import httpserver.response.Response;
-
 public enum Method {
-    GET(new GetResponder()),
-    POST(new PostResponder()),
-    PUT(new PutResponder()),
-    INVALID(new InvalidMethodResponder());
-
-    private final Responder responder;
-
-    Method(Responder responder) {
-        this.responder = responder;
-    }
-
-    public Response respond(AppConfig root, Request request) {
-        return responder.respond(root, request);
-    }
+    GET, POST, PUT
 }
