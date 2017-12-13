a435d62f85e2b3544af9526e1b83e2c0b600c937
Sam Szreter <s.szreter@gmail.com>
2017-12-14 00:59:03 +0200

    Change GetResponderTest to test interactions instead of integration

--- src/test/java/httpserver/responder/GetResponderTest.java
+++ src/test/java/httpserver/responder/GetResponderTest.java
@@ -1,6 +1,8 @@
 package httpserver.responder;
 
 import httpserver.AppConfig;
+import httpserver.file.Html;
+import httpserver.file.PathExaminer;
 import httpserver.header.Header;
 import httpserver.Request;
 import httpserver.response.Response;
@@ -9,26 +11,27 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import static httpserver.file.FileHelpers.tempDir;
-import static httpserver.file.FileHelpers.tempFileOptions;
-import static java.nio.file.Files.write;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class GetResponderTest {
 
     private final GetResponder getResponder;
-    private final Path root;
-    private final Path fileWithContents;
     private final AppConfig appConfigMock;
+    private final RouteMap routeMapMock;
+    private final PathExaminer pathExaminerMock;
+    private final Path rootMock;
+    private final Html htmlMock;
 
     public GetResponderTest() throws IOException {
-        getResponder = new GetResponder();
-        root = tempDir();
+        routeMapMock = mock(RouteMap.class);
+        pathExaminerMock = mock(PathExaminer.class);
+
+        htmlMock = mock(Html.class);
+        getResponder = new GetResponder(routeMapMock, pathExaminerMock, htmlMock);
         appConfigMock = mock(AppConfig.class);
-        when(appConfigMock.getRoot()).thenReturn(root);
-        fileWithContents = tempFileOptions(root, "aaa", ".gif");
-        write(fileWithContents, "Test file contents".getBytes());
+        rootMock = mock(Path.class);
+        when(appConfigMock.getRoot()).thenReturn(rootMock);
     }
 
     @Test
@@ -43,64 +46,58 @@ public class GetResponderTest {
     }
 
     @Test
-    public void getRequestForFile() throws Exception {
-        String fileName = fileWithContents.toString().substring(root.toString().length());
-        Request request = new Request("GET", fileName, new Header[0], "");
+    public void ifPathInRouteMapGetsResponderAndCallsRespond() throws Exception {
+        Responder responderMock = mock(Responder.class);
+        when(routeMapMock.contains("/example_route")).thenReturn(true);
+        when(routeMapMock.getResponder("/example_route")).thenReturn(responderMock);
+        Request request = new Request("GET", "/example_route", new Header[0], "");
 
-        Response response = getResponder.respond(appConfigMock, request);
+        getResponder.respond(appConfigMock, request);
 
-        Header[] expectedHeaders = new Header[]{new Header("Content-Type", "image/gif")};
-        assertEquals(200, response.getStatusCode());
-        assertEquals("Test file contents", new String(response.getPayload()));
-        assertEquals(expectedHeaders, response.getHeaders());
+        verify(routeMapMock).contains("/example_route");
+        verify(routeMapMock).getResponder("/example_route");
+        verify(responderMock).respond(appConfigMock, request);
     }
 
     @Test
-    public void getRequestForDir() throws Exception {
+    public void getsFileContentsForPath() throws Exception {
+        Path fullPathMock = mock(Path.class);
+        when(pathExaminerMock.getFullPath(rootMock, "/filename")).thenReturn(fullPathMock);
+        when(pathExaminerMock.pathExists(fullPathMock)).thenReturn(true);
+        when(pathExaminerMock.isFile(fullPathMock)).thenReturn(true);
+        byte[] payloadMock = new byte[0];
+        when(pathExaminerMock.fileContents(fullPathMock)).thenReturn(payloadMock);
+        Request request = new Request("GET", "/filename", new Header[0], "");
+
+        Response response = getResponder.respond(appConfigMock, request);
+
+        verify(appConfigMock).getRoot();
+        verify(pathExaminerMock).getFullPath(rootMock, "/filename");
+        verify(pathExaminerMock).pathExists(fullPathMock);
+        verify(pathExaminerMock).isFile(fullPathMock);
+        assertEquals(200, response.getStatusCode());
+        assertEquals(payloadMock, response.getPayload());
+        assertEquals("Content-Type", response.getHeaders()[0].getKey());
+    }
+
+    @Test
+    public void getsDirContentsForPath() throws Exception {
+        Path fullPathMock = mock(Path.class);
+        when(pathExaminerMock.getFullPath(rootMock, "/")).thenReturn(fullPathMock);
+        when(pathExaminerMock.pathExists(fullPathMock)).thenReturn(true);
+        when(pathExaminerMock.isFile(fullPathMock)).thenReturn(false);
+        Path path1 = mock(Path.class);
+        Path path2 = mock(Path.class);
+        Path[] pathArrayMock = new Path[]{path1, path2};
+        when(pathExaminerMock.directoryContents(fullPathMock)).thenReturn(pathArrayMock);
         Request request = new Request("GET", "/", new Header[0], "");
 
         Response response = getResponder.respond(appConfigMock, request);
 
+        verify(pathExaminerMock).isFile(fullPathMock);
+        verify(pathExaminerMock).directoryContents(fullPathMock);
+        verify(htmlMock).linkString(rootMock, path1);
+        verify(htmlMock).linkString(rootMock, path2);
         assertEquals(200, response.getStatusCode());
-        assertTrue(new String(response.getPayload()).contains("<a href="));
-    }
-
-    @Test
-    public void getRequestToCoffee() throws Exception {
-        Request request = new Request("GET", "/coffee", new Header[0], "");
-
-        Response response = getResponder.respond(appConfigMock, request);
-
-        assertEquals(418, response.getStatusCode());
-        assertTrue(new String(response.getPayload()).contains("I'm a teapot"));
-    }
-
-    @Test
-    public void getRequestToTea() throws Exception {
-        Request request = new Request("GET", "/tea", new Header[0], "");
-
-        Response response = getResponder.respond(appConfigMock, request);
-
-        assertEquals(200, response.getStatusCode());
-    }
-
-    @Test
-    public void getRequestToCookieWithQueryString() throws Exception {
-        Request request = new Request("GET", "/cookie", new Header[0], "type=chocolate");
-
-        Response response = getResponder.respond(appConfigMock, request);
-
-        assertEquals(200, response.getStatusCode());
-        assertEquals("Eat", new String(response.getPayload()));
-    }
-
-    @Test
-    public void getRequestToEatCookie() throws Exception {
-        Request request = new Request("GET", "/eat_cookie", new Header[0], "type=chocolate");
-
-        Response response = getResponder.respond(appConfigMock, request);
-
-        assertEquals(200, response.getStatusCode());
-        assertEquals("mmmm chocolate", new String(response.getPayload()));
     }
 }
