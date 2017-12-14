dbd2705a857526994e3e021fe478a0cfc2ae5802
Sam Szreter <s.szreter@gmail.com>
2017-12-14 14:28:30 +0200

    TDD PostResponder behaviour

--- src/main/java/httpserver/responder/PostResponder.java
+++ src/main/java/httpserver/responder/PostResponder.java
@@ -1,16 +1,42 @@
 package httpserver.responder;
 
 import httpserver.*;
+import httpserver.file.FileOperator;
+import httpserver.file.PathExaminer;
 import httpserver.response.MethodNotAllowedResponse;
 import httpserver.response.OkResponse;
 import httpserver.response.Response;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class PostResponder implements Responder {
+    private final PathExaminer pathExaminer;
+    private final FileOperator fileOperator;
+
+    public PostResponder(PathExaminer pathExaminer, FileOperator fileOperator) {
+        this.pathExaminer = pathExaminer;
+        this.fileOperator = fileOperator;
+    }
+
     @Override
-    public Response respond(AppConfig appConfig, Request request) {
-        if (request.getPathString().equals("/form")) {
-            return new OkResponse(new byte[0]);
+    public Response respond(AppConfig appConfig, Request request) throws IOException {
+        if (allowed(request.getPathString())) {
+            Path fullPath = pathExaminer.getFullPath(appConfig.getRoot(),
+                    request.getPathString());
+            if (pathExaminer.pathExists(fullPath)) {
+                fileOperator.replaceContents(fullPath, request.getBody().getBytes());
+                return new OkResponse(fileOperator.readContents(fullPath));
+            } else {
+                fileOperator.createFileAtPath(fullPath);
+                fileOperator.replaceContents(fullPath, request.getBody().getBytes());
+                return new OkResponse(fileOperator.readContents(fullPath));
+            }
         }
         return new MethodNotAllowedResponse();
+    }
+
+    private boolean allowed(String pathString) {
+        return pathString.equals("/form");
     }
 }
--- src/test/java/httpserver/responder/PostResponderTest.java
+++ src/test/java/httpserver/responder/PostResponderTest.java
@@ -1,13 +1,15 @@
 package httpserver.responder;
 
 import httpserver.AppConfig;
+import httpserver.file.FileOperator;
+import httpserver.file.PathExaminer;
 import httpserver.header.Header;
 import httpserver.Request;
 import httpserver.response.Response;
 import org.junit.Test;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static junit.framework.TestCase.*;
 import static org.mockito.Mockito.*;
@@ -15,29 +17,62 @@ import static org.mockito.Mockito.*;
 public class PostResponderTest {
 
     private final PostResponder postResponder;
-    private final Path root;
-    private final AppConfig appConfig;
+    private final AppConfig appConfigMock;
+    private final PathExaminer pathExaminerMock;
+    private final FileOperator fileOperatorMock;
+    private final Path rootMock;
+    private final Path fullPathMock;
+    private final byte[] fileContentsMock;
 
     public PostResponderTest() {
-        postResponder = new PostResponder();
-        root = Paths.get("test");
-        appConfig = mock(AppConfig.class);
+        pathExaminerMock = mock(PathExaminer.class);
+        fileOperatorMock = mock(FileOperator.class);
+        postResponder = new PostResponder(pathExaminerMock, fileOperatorMock);
+        rootMock = mock(Path.class);
+        appConfigMock = mock(AppConfig.class);
+        when(appConfigMock.getRoot()).thenReturn(rootMock);
+        fullPathMock = mock(Path.class);
+        fileContentsMock = new byte[0];
     }
 
     @Test
-    public void gets200StatusCode() throws Exception {
-        Request request = new Request("POST", "/form", new Header[0], "");
+    public void overwritesFileWithDataIfAllowedExists() throws Exception {
+        String pathString = "/form";
+        when(pathExaminerMock.pathExists(any())).thenReturn(true);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        when(fileOperatorMock.readContents(fullPathMock)).thenReturn(fileContentsMock);
+        Request request = new Request("POST", pathString, new Header[0], "", "data=example");
 
-        Response response = postResponder.respond(appConfig, request);
+        Response response = postResponder.respond(appConfigMock, request);
 
         assertEquals(200, response.getStatusCode());
+        verify(fileOperatorMock).replaceContents(fullPathMock, "data=example".getBytes());
+        verify(fileOperatorMock).readContents(fullPathMock);
+        assertEquals(fileContentsMock, response.getPayload());
     }
 
     @Test
-    public void returns405IfNotFormUrl() throws Exception {
-        Request request = new Request("POST", "/file1.txt", new Header[0], "");
+    public void createsFileWithDataIfAllowedAndDoesntExist() throws Exception {
+        String pathString = "/form";
+        when(pathExaminerMock.pathExists(any())).thenReturn(false);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        when(fileOperatorMock.readContents(fullPathMock)).thenReturn(fileContentsMock);
+        Request request = new Request("POST", pathString, new Header[0], "", "data=example");
 
-        Response response = postResponder.respond(appConfig, request);
+        Response response = postResponder.respond(appConfigMock, request);
+
+        assertEquals(200, response.getStatusCode());
+        verify(fileOperatorMock).createFileAtPath(fullPathMock);
+        verify(fileOperatorMock).replaceContents(fullPathMock, "data=example".getBytes());
+        verify(fileOperatorMock).readContents(fullPathMock);
+        assertEquals(fileContentsMock, response.getPayload());
+    }
+
+    @Test
+    public void returns405IfNotAllowed() throws Exception {
+        Request request = new Request("POST", "/not_allowed", new Header[0], "", "data=example");
+
+        Response response = postResponder.respond(appConfigMock, request);
 
         assertEquals(405, response.getStatusCode());
     }
