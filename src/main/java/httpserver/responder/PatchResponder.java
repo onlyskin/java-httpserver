1ffa7b3081f87764eab767a2df4eeca55538031b
Sam Szreter <s.szreter@gmail.com>
2017-12-18 17:58:40 +0200

    Add PatchResponder

---
+++ src/main/java/httpserver/responder/PatchResponder.java
@@ -0,0 +1,59 @@
+package httpserver.responder;
+
+import httpserver.AppConfig;
+import httpserver.Hasher;
+import httpserver.Request;
+import httpserver.file.FileOperator;
+import httpserver.file.PathExaminer;
+import httpserver.response.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class PatchResponder implements Responder {
+    private PathExaminer pathExaminer;
+    private final FileOperator fileOperator;
+    private final Hasher hasher;
+
+    public PatchResponder(PathExaminer pathExaminer,
+                          FileOperator fileOperator, Hasher hasher) {
+        this.pathExaminer = pathExaminer;
+        this.fileOperator = fileOperator;
+        this.hasher = hasher;
+    }
+
+    @Override
+    public Response respond(AppConfig appConfig, Request request) throws IOException {
+        if (allowed(request.getPathString())) {
+            Path fullPath = pathExaminer.getFullPath(appConfig.getRoot(), request.getPathString());
+            if (pathExaminer.pathExists(fullPath)) {
+                if (noIfMatchHeader(request)) {
+                    return new ConflictResponse();
+                }
+                if (matchingHash(fullPath, request)) {
+                    byte[] newFileContents = request.getBody().getBytes();
+                    fileOperator.appendToFile(fullPath, newFileContents);
+                    return new NoContentResponse(hasher.getHash(newFileContents));
+                }
+                return new ConflictResponse();
+            } else {
+                return new NotFoundResponse();
+            }
+        }
+        return new MethodNotAllowedResponse();
+    }
+
+    private boolean noIfMatchHeader(Request request) {
+        return !request.hasHeader("If-Match");
+    }
+
+    private boolean matchingHash(Path fullPath, Request request) {
+        byte[] fileContents = pathExaminer.fileContents(fullPath);
+        String ifMatchHash = request.getHeaderValue("If-Match");
+        return hasher.matches(fileContents, ifMatchHash);
+    }
+
+    public boolean allowed(String pathString) {
+        return pathString.equals("/patch-content.txt");
+    }
+}
---
+++ src/test/java/httpserver/responder/PatchResponderTest.java
@@ -0,0 +1,121 @@
+package httpserver.responder;
+
+import httpserver.AppConfig;
+import httpserver.Hasher;
+import httpserver.Request;
+import httpserver.file.FileOperator;
+import httpserver.file.PathExaminer;
+import httpserver.header.Header;
+import httpserver.response.Response;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class PatchResponderTest {
+    private final PathExaminer pathExaminerMock;
+    private final FileOperator fileOperatorMock;
+    private final AppConfig appConfigMock;
+    private final Path rootMock;
+    private final Path fullPathMock;
+    private final byte[] fileContentsMock;
+    private final Hasher hasherMock;
+    private PatchResponder patchResponder;
+
+    public PatchResponderTest() {
+        pathExaminerMock = mock(PathExaminer.class);
+        fileOperatorMock = mock(FileOperator.class);
+        hasherMock = mock(Hasher.class);
+        this.patchResponder = new PatchResponder(pathExaminerMock, fileOperatorMock, hasherMock);
+        rootMock = mock(Path.class);
+        appConfigMock = mock(AppConfig.class);
+        when(appConfigMock.getRoot()).thenReturn(rootMock);
+        fullPathMock = mock(Path.class);
+        fileContentsMock = new byte[0];
+    }
+
+    @Test
+    public void returns409IfNoIfMatchHeader() throws Exception {
+        String pathString = "/patch-content.txt";
+        when(pathExaminerMock.pathExists(any())).thenReturn(true);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        when(hasherMock.matches(any(), any())).thenReturn(true);
+        Header[] headers = new Header[0];
+        Request request = new Request("PATCH", pathString, headers, "", "patch contents");
+
+        Response response = patchResponder.respond(appConfigMock, request);
+
+        assertEquals(409, response.getStatusCode());
+    }
+
+    @Test
+    public void callsHasherReturns204AndUpdatesFileWithRequestBodyIfHashesMatch() throws Exception {
+        String pathString = "/patch-content.txt";
+        when(pathExaminerMock.pathExists(any())).thenReturn(true);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        when(pathExaminerMock.fileContents(fullPathMock)).thenReturn("mock text".getBytes());
+        when(hasherMock.matches(any(), any())).thenReturn(true);
+        when(hasherMock.getHash(any())).thenReturn("hash code");
+        String hashMock = "bfc13a";
+        Header[] headers = new Header[]{new Header("If-Match", hashMock)};
+        Request request = new Request("PATCH", pathString, headers, "", "patch contents");
+
+        Response response = patchResponder.respond(appConfigMock, request);
+
+        verify(pathExaminerMock).fileContents(fullPathMock);
+        verify(hasherMock).matches("mock text".getBytes(), hashMock);
+        verify(fileOperatorMock).appendToFile(fullPathMock, "patch contents".getBytes());
+        assertEquals(204, response.getStatusCode());
+        Header[] expected = new Header[]{new Header("ETag", "hash code")};
+        assertTrue(Arrays.equals(expected, response.getHeaders()));
+    }
+
+    @Test
+    public void returns409IfHashesDontMatch() throws Exception {
+        String pathString = "/patch-content.txt";
+        when(pathExaminerMock.pathExists(any())).thenReturn(true);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        when(pathExaminerMock.fileContents(fullPathMock)).thenReturn("mock text".getBytes());
+        when(hasherMock.matches(any(), any())).thenReturn(false);
+        String hashMock = "bfc13a";
+        Header[] headers = new Header[]{new Header("If-Match", hashMock)};
+        Request request = new Request("PATCH", pathString, headers, "", "patch contents");
+
+        Response response = patchResponder.respond(appConfigMock, request);
+
+        verify(hasherMock).matches("mock text".getBytes(), hashMock);
+        assertEquals(409, response.getStatusCode());
+    }
+
+    @Test
+    public void returns404IfAllowedButDoesntExist() throws Exception {
+        String pathString = "/patch-content.txt";
+        when(pathExaminerMock.pathExists(any())).thenReturn(false);
+        when(pathExaminerMock.getFullPath(rootMock, pathString)).thenReturn(fullPathMock);
+        Request request = new Request("PATCH", pathString, new Header[0], "", "data=example");
+
+        Response response = patchResponder.respond(appConfigMock, request);
+
+        assertEquals(404, response.getStatusCode());
+    }
+
+    @Test
+    public void returns405IfNotAllowed() throws Exception {
+        Request request = new Request("PATCH", "/not_allowed", new Header[0], "", "data=example");
+
+        Response response = patchResponder.respond(appConfigMock, request);
+
+        assertEquals(405, response.getStatusCode());
+    }
+
+    @Test
+    public void PathContentTxtIsAllowed() throws Exception {
+        assertTrue(patchResponder.allowed("/patch-content.txt"));
+        assertFalse(patchResponder.allowed("/other"));
+    }
+}
