49f680d0f653024d1226a7640b128348c726695b
Sam Szreter <s.szreter@gmail.com>
2017-12-20 14:54:23 +0200

    Inline variables

--- src/main/java/httpserver/App.java
+++ src/main/java/httpserver/App.java
@@ -9,26 +9,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class App {
-
-    private static ExecutorService threadPool;
-    private static ServerFactory serverFactory;
-    private static SocketHandlerFactory socketHandlerFactory;
-    private static PathExaminer pathExaminer;
-    private static Path root;
-
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[1]);
         String fileDirectory = args[3];
 
-        pathExaminer = new PathExaminer();
-        root = pathExaminer.getPath(fileDirectory);
+        PathExaminer pathExaminer = new PathExaminer();
+        Path root = pathExaminer.getPath(fileDirectory);
         Path logPath = pathExaminer.concatenate(root,"logs");
         AppConfig appConfig = new AppConfig(root, new Logger(logPath, new FileOperator()));
 
-        serverFactory = new ServerFactory(new ServerSocketFactory(), appConfig);
-        socketHandlerFactory = new SocketHandlerFactory();
+        ServerFactory serverFactory = new ServerFactory(new ServerSocketFactory(), appConfig);
+        SocketHandlerFactory socketHandlerFactory = new SocketHandlerFactory();
 
-        threadPool = Executors.newFixedThreadPool(16);
+        ExecutorService threadPool = Executors.newFixedThreadPool(16);
 
         Server server = serverFactory.makeServer(port);
         while (true) {
